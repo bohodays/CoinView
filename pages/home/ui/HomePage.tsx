b26fec835b567ec1a5dc5e9e7760bd7c6f57014c
bohodays <bohodays0316@gmail.com>
2026-02-19 21:45:52 +0900

    design: 메인페이지 coin search bar UI 구현

--- pages/home/ui/HomePage.tsx
+++ pages/home/ui/HomePage.tsx
@@ -1,13 +1,14 @@
+import CoinSearchBar from "@/feature/coin-search/ui/CoinSearchBar";
 import Header from "@/widgets/header/ui/Header";
 
 const HomePage = () => {
   return (
-    <div>
+    <div className="flex flex-col h-full gap-10">
       {/* 1. Header */}
       <Header />
 
       {/* 2. SearchBar */}
-
+      <CoinSearchBar />
       {/* 3. CoinList */}
     </div>
   );
