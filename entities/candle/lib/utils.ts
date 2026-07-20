8306df386aaf41a5ece8265dee94384aa2b89be0
bohodays <bohodays0316@gmail.com>
2026-07-20 20:11:43 +0900

    refactor: minutesUnitTransWebSocketType 8-case switch를 한 줄로 축소
    동일 결과를 내는 8개 case 분기를 candle.\${minutesUnit}m 템플릿
    리터럴로 대체. 동작은 기존과 동일(seconds -> candle.1s, minutes+값 ->
    candle.{n}m, 그 외 -> "candle.").

--- entities/candle/lib/utils.ts
+++ entities/candle/lib/utils.ts
@@ -14,43 +14,9 @@ export function minutesUnitTransWebSocketType(
   candleUnit: CandleUnit,
   minutesUnit?: MinutesUnit,
 ) {
-  // 초봉
   if (candleUnit === "seconds") return "candle.1s";
-
-  // 분봉
-  let typeResult = "candle.";
-  if (candleUnit === "minutes") {
-    switch (minutesUnit) {
-      case "1":
-        typeResult += "1m";
-        break;
-      case "3":
-        typeResult += "3m";
-        break;
-      case "5":
-        typeResult += "5m";
-        break;
-      case "10":
-        typeResult += "10m";
-        break;
-      case "15":
-        typeResult += "15m";
-        break;
-      case "30":
-        typeResult += "30m";
-        break;
-      case "60":
-        typeResult += "60m";
-        break;
-      case "240":
-        typeResult += "240m";
-        break;
-      default:
-        break;
-    }
-  }
-
-  return typeResult;
+  if (candleUnit === "minutes" && minutesUnit) return `candle.${minutesUnit}m`;
+  return "candle.";
 }
 
 export function isCandleWebSocketSupported(
